fc95259c1368cb7d04d993d31ab8ead696cd4075
Numan Safi <numansafi@live.com>
2021-08-17 16:56:59 +0100

    Add completed column to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_155215) do
+ActiveRecord::Schema.define(version: 2021_08_17_155406) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
